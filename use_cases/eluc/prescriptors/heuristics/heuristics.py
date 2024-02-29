c07344d3fc886de6eebd66d9a0e5bcd6aa099c0d
Daniel Young <danyoung@utexas.edu>
2024-02-29 13:33:09 -0800

    Vectorized perfect heuristic. Updated experiments to work with new heuristic

--- use_cases/eluc/prescriptors/heuristics/heuristics.py
+++ use_cases/eluc/prescriptors/heuristics/heuristics.py
@@ -1,7 +1,6 @@
 """
 Heuristic to compare our prescriptors to.
 """
-
 from abc import ABC, abstractmethod
 
 import pandas as pd
@@ -18,24 +17,25 @@ class HeuristicPrescriptor(Prescriptor, ABC):
     Requires an implementation of reco_heuristic which takes a context dataframe and returns
     recommendations based on the heuristic.
     """
-    def __init__(self, pct: float, predictor: Predictor):
-        self.pct = pct
+    def __init__(self, predictor: Predictor):
         self.predictor = predictor
 
     @abstractmethod
-    def _reco_heuristic(self, context_df: pd.DataFrame) -> pd.DataFrame:
+    def _reco_heuristic(self, pct: float, context_df: pd.DataFrame) -> pd.DataFrame:
         """
-        Abstract method that takes a context dataframe and returns a dataframe of recommendations.
+        Abstract method that takes a percentage threshold of land change and a 
+        context dataframe and returns a dataframe of recommendations based on the heuristic.
         """
         raise NotImplementedError
     
-    def prescribe_land_use(self, _, __, context_df: pd.DataFrame) -> pd.DataFrame:
+    def prescribe_land_use(self, pct, __, context_df: pd.DataFrame) -> pd.DataFrame:
         """
+        TODO: The header of this function is weird. Not entirely sure what to do about it.
         The header of this function is kinda gacky. Not sure what to do about this...
         We don't need a result dir or candidate id to load a prescriptor because we 
         prescribe via. heuristic.
         """
-        reco_df = self._reco_heuristic(context_df)
+        reco_df = self._reco_heuristic(pct, context_df)
         prescribed_actions_df = reco_df[constants.RECO_COLS] - context_df[constants.RECO_COLS]
 
         # Rename the columns to match what the predictor expects
@@ -57,19 +57,19 @@ class EvenHeuristic(HeuristicPrescriptor):
     """
     Implementation of HeuristicPrescriptor that evenly distributes land use to a "best" column.
     """
-    def __init__(self, pct: float, best_col: str, predictor: Predictor):
-        super().__init__(pct, predictor)
+    def __init__(self, best_col: str, predictor: Predictor):
+        super().__init__(predictor)
         self.best_col = best_col
         self.presc_cols = [col for col in constants.RECO_COLS if col != best_col]
     
-    def _reco_heuristic(self, context_df: pd.DataFrame):
+    def _reco_heuristic(self, pct: float, context_df: pd.DataFrame):
         """
         Takes evenly from all columns and adds to best col.
         Removes land_use * (total_change / changeable_land) so we remove proportionally
         rather than truly evenly.
         """
         adjusted = context_df.copy()
-        adjusted["scaled_change"] = self.pct * adjusted[constants.LAND_USE_COLS].sum(axis=1)
+        adjusted["scaled_change"] = pct * adjusted[constants.LAND_USE_COLS].sum(axis=1)
         adjusted["row_sum"] = adjusted[self.presc_cols].sum(axis=1)
         to_change = adjusted["row_sum"] > 0
         adjusted["max_change"] = adjusted[["scaled_change", "row_sum"]].min(axis=1)
@@ -86,17 +86,16 @@ class PerfectHeuristic(HeuristicPrescriptor):
     Implementation of HeuristicPrescriptor that does an informed land use prescription 
     based on linear regression coefficients.
     """
-    def __init__(self, pct: float, coefs: list[float], predictor: Predictor):
+    def __init__(self, coefs: list[float], predictor: Predictor):
         """
         We save and sort the columns by highest coefficient i.e. most emissions.
         Separate the best column according to the coefficients to add to.
         """
-        super().__init__(pct, predictor)
+        super().__init__(predictor)
         assert len(coefs) == len(constants.RECO_COLS)
         # Sort columns by coefficient
         reco_cols = [col for col in constants.RECO_COLS]
-        reco_coefs = coefs
-        zipped = zip(reco_cols, reco_coefs)
+        zipped = zip(reco_cols, coefs)
         sorted_zip = sorted(zipped, key=lambda x: x[1], reverse=True)
         self.reco_cols, _ = zip(*sorted_zip)
         self.reco_cols = list(self.reco_cols)
@@ -105,30 +104,24 @@ class PerfectHeuristic(HeuristicPrescriptor):
         self.best_col = self.reco_cols[-1]
         self.reco_cols.pop()
 
-    def _reco_heuristic(self, context_df: pd.DataFrame):
+    def _reco_heuristic(self, pct: float, context_df: pd.DataFrame) -> pd.DataFrame:
         """
         Perfect prescription algorithm:
-            1. Add to forest as much pct change as possible up to forest = 1
-            2. Subtract from bottom up however much was changed
-        TODO: Currently we do this row-wise. We could try to do this column-wise using
-        vectorized functions but it was taking a long time for me to figure out.
+            1. Subtract up to scaled change starting from worst column.
+            2. Add to best column however much was removed.
+        It is done in a vectorized way to massively speed up computation.
         """
         adjusted = context_df.copy()
-        pct = self.pct
-        for index, row in adjusted.iterrows():
-            scaled_change = pct * row[constants.LAND_USE_COLS].sum()
-            presc_sum = row[self.reco_cols].sum()
-            max_change = min(scaled_change, presc_sum)
-            for col in self.reco_cols:
-                # If there is more land use than change left, set remaining change to 0 and finish.
-                if row[col] >= max_change:
-                    adjusted.at[index, col] -= max_change
-                    break
-                # If there is less land use than change left, set land use to 0 and reduce change.
-                elif row[col] > 0:
-                    max_change -= row[col]
-                    adjusted.at[index, col] = 0
-            # Add to the best column.
-            adjusted.at[index, self.best_col] += min(scaled_change, presc_sum)
-        
+
+        adjusted["scaled_change"] = pct * adjusted[constants.LAND_USE_COLS].sum(axis=1)
+        adjusted["presc_sum"] = adjusted[self.reco_cols].sum(axis=1)
+        adjusted["to_change"] = adjusted[["scaled_change", "presc_sum"]].min(axis=1)
+
+        for col in self.reco_cols:
+            change = adjusted[[col, "to_change"]].min(axis=1)
+            adjusted[col] -= change
+            adjusted["to_change"] -= change
+
+        adjusted[self.best_col] += adjusted[["scaled_change", "presc_sum"]].min(axis=1)
+        adjusted = adjusted.drop(["scaled_change", "presc_sum", "to_change"], axis=1)
         return adjusted

a241979ba90a1e6e9ffd5fd388cb81e143eada1f
Daniel <danyoung@utexas.edu>
2024-03-13 10:02:10 -0700

    Updated predictor significance with fixed dataset!

--- use_cases/eluc/experiments/predictor_significance.py
+++ use_cases/eluc/experiments/predictor_significance.py
@@ -24,45 +24,51 @@ def train_and_test(n: int, model_constructor, config: dict, train_df: pd.DataFra
         (This is currently only used for the random forest)
     """
     countries_df = construct_countries_df()
-
+    print(f"Saving to: {save_path}")
     save_dir = save_path.parent
     save_dir.mkdir(parents=True, exist_ok=True)
-    if not save_path.is_file():
-        with open(save_path, "w") as f:
-            f.write("region,test,mae,time\n")
-    with open(save_path, "a") as f:
-        # Iterate over all regions
-        for train_region in train_regions:
-            print(train_region)
-            if train_region != "ALL":
-                countries = constants.COUNTRY_DICT[train_region]
-                idx = countries_df[countries_df["abbrevs"].isin(countries)].index.values
-                train_region_df = train_df[train_df["country"].isin(idx)]
-            else:
-                train_region_df = train_df
-            
-            # n times for each region
-            for i in tqdm(range(n)):
-                model = model_constructor(**config)
-                s = time.time()
-                _ = model.fit(train_region_df, train_region_df["ELUC"])
-                e = time.time()
-                # Evaluate on each region
-                for test_region in constants.COUNTRY_DICT.keys():
-                    if test_region != "ALL":
-                        countries = constants.COUNTRY_DICT[test_region]
-                        idx = countries_df[countries_df["abbrevs"].isin(countries)].index.values
-                        test_region_df = test_df[test_df["country"].isin(idx)]
-                    else:
-                        test_region_df = test_df
-                        if override_start_year:
-                            test_region_df = test_region_df.loc[override_start_year:]
-                    
-                    mae = mean_absolute_error(model.predict(test_region_df), test_region_df["ELUC"])
-                    f.write(f"{train_region},{test_region},{mae},{e - s}\n")
+
+    # Iterate over all regions
+    results = []
+    for train_region in train_regions:
+        print(f"Training on {train_region}")
+        if train_region != "ALL":
+            countries = constants.COUNTRY_DICT[train_region]
+            idx = countries_df[countries_df["abbrevs"].isin(countries)].index.values
+            train_region_df = train_df[train_df["country"].isin(idx)]
+        else:
+            train_region_df = train_df
+            if override_start_year:
+                print(f"Overriding start year to: {override_start_year}")
+                train_region_df = train_region_df.loc[override_start_year:]
+        
+        # n times for each region
+        for _ in tqdm(range(n)):
+            result_row = {"train": train_region}
+            model = model_constructor(**config)
+            s = time.time()
+            _ = model.fit(train_region_df, train_region_df["ELUC"])
+            e = time.time()
+            result_row["time"] = e - s
+            # Evaluate on each region
+            for test_region, countries in constants.COUNTRY_DICT.items():
+                if test_region != "ALL":
+                    idx = countries_df[countries_df["abbrevs"].isin(countries)].index.values
+                    test_region_df = test_df[test_df["country"].isin(idx)]
+                else:
+                    test_region_df = test_df
+                
+                mae = mean_absolute_error(model.predict(test_region_df), test_region_df["ELUC"])
+                result_row[test_region] = mae
+
+            results.append(result_row)
+        
+    results_df = pd.DataFrame(results)
+    results_df.to_csv(save_path)
 
 if __name__ == "__main__":
 
+    print("Loading data...")
     dataset = ELUCData()
 
     nn_config = {
@@ -89,13 +95,10 @@ if __name__ == "__main__":
     model_constructors = [NeuralNetPredictor, RandomForestPredictor, LinearRegressionPredictor]
     configs = [nn_config, forest_config, linreg_config]
     model_names = ["neural_network", "random_forest", "linear_regression"]
-    #train_regions = constants.COUNTRY_DICT.keys()
-    train_regions = ["US", "ALL"]
-    significance_path = Path("experiments/predictor_significance/no_overlap")
-    for model_constructor, config, model_name in zip(model_constructors[1:2], configs[1:2], model_names[1:2]):
-        override_start_year = None
-        if model_name == "random_forest":
-            override_start_year = 1982
+    train_regions = list(constants.COUNTRY_DICT.keys())
+    significance_path = Path("experiments/predictor_significance/no_overlap_fixed")
+    for model_constructor, config, model_name in zip(model_constructors, configs, model_names):
+        override_start_year = None if model_name != "random_forest" else 1982
         print(model_name)
         train_and_test(30,
                     model_constructor,

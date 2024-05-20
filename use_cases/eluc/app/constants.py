b2b7189e7b17391206a09e1f426d034c9a7317ff
Daniel Young <danyoung@utexas.edu>
2024-05-20 09:23:20 -0700

    Tweaked constants and got rid of old predictor/prescriptor

--- use_cases/eluc/app/constants.py
+++ use_cases/eluc/app/constants.py
@@ -1,24 +1,15 @@
-import base64
 import os
+from pathlib import Path
 
-ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-DATA_FILE_PATH = os.path.join(ROOT_DIR, "../data/processed/app_data.csv")
+from data.constants import LAND_USE_COLS
+
+DATA_FILE_PATH = Path("data/processed/app_data.csv")
 
 APP_START_YEAR = 2012
 
 GRID_STEP = 0.25
 
 INDEX_COLS = ["time", "lat", "lon"]
-
-LAND_USE_COLS = ['crop', 'pastr', 'primf', 'primn', 'range', 'secdf', 'secdn', 'urban']
-CONTEXT_COLUMNS = LAND_USE_COLS + ['cell_area', 'time', 'lat', 'lon']
-DIFF_LAND_USE_COLS = [f"{col}_diff" for col in LAND_USE_COLS]
-COLS_MAP = dict(zip(LAND_USE_COLS, DIFF_LAND_USE_COLS))
-
-# Prescriptor outputs
-RECO_COLS = ['c3ann', 'c3nfx', 'c3per','c4ann', 'c4per', 'pastr', 'range', 'secdf', 'secdn']
-DIFF_RECO_COLS = [f"{col}_diff" for col in RECO_COLS]
-RECO_MAP = dict(zip(RECO_COLS, DIFF_RECO_COLS))
 
 NO_CHANGE_COLS = ["primf", "primn", "urban"]
 CHART_COLS = LAND_USE_COLS + ["nonland"]
@@ -36,14 +27,14 @@ FIELDS = ['pastr', 'range']
 
 CHART_TYPES = ["Treemap", "Pie Chart"]
 
-PREDICTOR_PATH = os.path.join(ROOT_DIR, "../predictors/")
-PRESCRIPTOR_PATH = os.path.join(ROOT_DIR, "../prescriptors/")
+PREDICTOR_PATH = Path("predictors/trained_models")
+PRESCRIPTOR_PATH = Path("prescriptors/nsga2/app_prescriptors")
 
 # Pareto front
 # PARETO_CSV_PATH = os.path.join(PRESCRIPTOR_PATH, "pareto.csv")
 # PARETO_FRONT_PATH = os.path.join(PRESCRIPTOR_PATH, "pareto_front.png")
 # PARETO_FRONT = base64.b64encode(open(PARETO_FRONT_PATH, 'rb').read()).decode('ascii')
 
-FIELDS_PATH = os.path.join(PRESCRIPTOR_PATH, "fields.json")
+FIELDS_PATH = PRESCRIPTOR_PATH / "fields.json"
 
 DEFAULT_PRESCRIPTOR_IDX = 3  # By default we select the fourth prescriptor that minimizes change

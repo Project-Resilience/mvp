afd61f70a8b9426d6686df7dd78e1a9a353eaba6
Daniel Young <danyoung@utexas.edu>
2024-10-08 11:25:00 -0700

    Added references to Neuro and PR

--- use_cases/eluc/app/components/intro.py
+++ use_cases/eluc/app/components/intro.py
@@ -16,13 +16,19 @@ class IntroComponent():
             className="mb-5 d-flex flex-column align-items-center justify-content-center",
             children=[
                 html.H2("Land Use Optimization", className="text-center w-100 mb-5 mt-5"),
-                html.P("Allocation of land for different uses significantly affects carbon balance and climate \
-                       change. A surrogate model learned from historical land-use changes and carbon \
-                       emission simulations allows efficient evaluation of such allocations. An evolutionary \
-                       search then discovers effective land-use policies for specific locations. This \
-                       system, built on the Project Resilience platform, generates Pareto fronts trading off\
-                       carbon impact and amount of change customized to different locations, offering a \
-                       useful tool for land-use planning.", className="w-50")
+                html.P([
+                    "Allocation of land for different uses significantly affects carbon balance and climate \
+                    change. A surrogate model learned from historical land-use changes and carbon \
+                    emission simulations allows efficient evaluation of such allocations. An evolutionary \
+                    search then discovers effective land-use policies for specific locations. This \
+                    system, using the technology behind ",
+                    html.A("Cognizant NeuroAI", href="https://www.cognizant.com/us/en/services/ai/ai-lab"),
+                    " and built on the ",
+                    html.A("Project Resilience platform",
+                           href="https://www.itu.int/en/ITU-T/extcoop/ai-data-commons/Pages/project-resilience.aspx"),
+                    ", generates Pareto fronts trading off carbon impact and amount of change customized to different \
+                    locations, offering a useful tool for land-use planning."
+                ], className="w-50"),
             ]
         )
 
